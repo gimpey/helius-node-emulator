05ef4eff22abe014fd2e74f7214ae02b9841bbc0
gimpey <gimpey@pr0xy.io>
2024-12-19 18:12:32 +0000

    adding a system proto for system message transfers

--- build.rs
+++ build.rs
@@ -2,8 +2,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .compile_protos(
             &[
-                "protos/spl_token_creation.proto",
+                "protos/spl_token.proto",
                 "protos/daos_fund.proto",
+                "protos/system.proto",
             ],
             &["protos"],
         )?;
