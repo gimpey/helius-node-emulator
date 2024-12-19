fea1360fbdca60f13d3c591eb75c1272a6dc26eb
gimpey <gimpey@pr0xy.io>
2024-12-19 23:42:15 +0000

    adding serum market initializing

--- src/constants/zmq.rs
+++ src/constants/zmq.rs
@@ -4,3 +4,5 @@ pub const PUMP_FUN_BONDING_CURVE_UPDATE: &str = "pump_fun_bonding_curve_update";
 
 pub const LAMPORTS_BALANCE_UPDATE: &str = "lamports_balance_update";
 pub const SPL_TOKEN_BALANCE_UPDATE: &str = "spl_token_balance_update";
+
+pub const SERUM_INITIALIZE_MARKET_UPDATE: &str = "serum_initialize_market_update";
--- src/instructions/serum/initialize_market.rs
+++ src/instructions/serum/initialize_market.rs
@@ -1,63 +1,117 @@
+use prost::Message;
+/// # Serum Market ID Creation (Initialize Market)
+/// 
+/// REQUIRES REDIS: FALSE
+/// REQUIRES ZMQ: TRUE
+///
 /// Data Deserialization: https://github.com/project-serum/serum-ts/blob/master/packages/serum/src/instructions.js#L33
 /// 
 /// Accounts: https://github.com/project-serum/serum-ts/blob/master/packages/serum/src/instructions.js#L195
+/// 
+/// https://github.com/project-serum/serum-dex/blob/master/dex/src/instruction.rs#L341
 
 use solana_transaction_status::{parse_accounts::ParsedAccount, UiPartiallyDecodedInstruction};
+use borsh::{BorshDeserialize, BorshSerialize};
 use tokio::sync::mpsc::UnboundedSender;
 use tracing::info;
 use yansi::Paint;
 
-use crate::messaging::MpscMessage;
+use crate::{
+    constants::{addresses::{
+        PUMP_FUN_RAYDIUM_MIGRATION, 
+        WSOL_ADDRESS
+    }, zmq::SERUM_INITIALIZE_MARKET_UPDATE}, 
+    messaging::MpscMessage
+};
 
-// Need to figure out the difference between these two:
-// 
-// https://solscan.io/tx/2z7wMLr6hCZnwi3yL7PVfNwazxBmkKAXb3mGrsivsEN4xdrYanWsZj25pjS34FDmj3D2o8ceADC9c9pgARmA5tH7
-// https://solscan.io/tx/njmt4jVBBRsW1ERZ86QTnX2YoRGcCM3LwZjxXpQfU6C9RgdteNy8PoGCCasERHs5nKNoUkpG3UTREgjaz7kcss1
-//
-// This shows what it should be:
-// https://github.com/project-serum/serum-dex/blob/master/dex/src/instruction.rs#L341
+pub mod spl_token {
+    tonic::include_proto!("spl_token");
+}
+
+use spl_token::SerumMarketCreation;
+
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+struct InitializeMarketData {
+    version: u8,
+    discriminator: u32,
+    base_lot_size: u64,
+    quote_lot_size: u64,
+    fee_rate_bps: u16,
+    vault_signer_nonce: u64,
+    quote_dust_threshold: u64,
+}
+
+impl InitializeMarketData {
+    pub fn from_base58_data(data: &String) -> Self {
+        let decoded_data = bs58::decode(data).into_vec().unwrap();
+        Self::try_from_slice(&decoded_data).unwrap()
+    }
+}
+
 
 pub fn initialize_market_handler(
     instruction: &UiPartiallyDecodedInstruction,
-    _accounts: &Vec<ParsedAccount>,
-    _tx: UnboundedSender<MpscMessage>,
+    accounts: &Vec<ParsedAccount>,
+    tx: UnboundedSender<MpscMessage>,
     _signature: &String
 ) {
-    let token_address = instruction.accounts.get(7).unwrap();
+    let is_pump_fun = accounts.iter()
+        .find(|account| account.pubkey == PUMP_FUN_RAYDIUM_MIGRATION)
+        .map(|account| account.signer)
+        .unwrap_or(false);
 
-    info!("{:#?}", instruction);
+    let is_official: &str = if is_pump_fun { "OFFICIAL PUMP.FUN" } else { "" };
 
-    info!(
-        "Processing {} instruction for {} {}", 
-        Paint::magenta("INITIALIZE_MARKET"), 
-        Paint::cyan("SERUM_PROGRAM"),
-        Paint::black(token_address.to_string())
-    );
-    info!("{}", Paint::red(_signature));
+    if is_pump_fun {
+        let data = InitializeMarketData::from_base58_data(&instruction.data);
 
-    // destructure the data to the vault signer nonce
-    // instruction.accounts.get(0) is the serum market id address
-    // instruction.accounts.get(2) is the serum event queue "
-    // instruction.accounts.get(3) is the serum bids "
-    // instruction.accounts.get(4) is the serum asks "
-    // instruction.accounts.get(5) is the serum coin vault "
-    // instruction.accounts.get(6) is the serum pc vault "
-    // instruction.accounts.get(7) is the mint "
+        let market_id_address = instruction.accounts.get(0).unwrap();
+        let request_queue_address = instruction.accounts.get(1).unwrap();
+        let event_queue_address = instruction.accounts.get(2).unwrap();
+        let bids_address = instruction.accounts.get(3).unwrap();
+        let asks_address = instruction.accounts.get(4).unwrap();
+        let base_spl_token_account_address = instruction.accounts.get(5).unwrap();
+        let quote_spl_token_account_address = instruction.accounts.get(6).unwrap();
+        let base_token_address = instruction.accounts.get(7).unwrap();
+        let quote_token_address = instruction.accounts.get(8).unwrap();
+    
+        let base = if base_token_address == WSOL_ADDRESS { "WSOL" } else { base_token_address };
+        let quote = if quote_token_address == WSOL_ADDRESS { "WSOL" } else { quote_token_address };
 
-    // todo: ensure the sender is pump.fun (I guess someone could create one)?
-    // todo: parse which accounts are which
-    // todo: cache them
+        let message = SerumMarketCreation {
+            market_id_address: market_id_address.to_string(),
+            request_queue_address: request_queue_address.to_string(),
+            event_queue_address: event_queue_address.to_string(),
+            bids_address: bids_address.to_string(),
+            asks_address: asks_address.to_string(),
+            base_spl_token_account_address: base_spl_token_account_address.to_string(),
+            quote_spl_token_account_address: quote_spl_token_account_address.to_string(),
+            base_token_address: base_token_address.to_string(),
+            quote_token_address: quote_token_address.to_string(),
+            base_lot_size: data.base_lot_size,
+            quote_lot_size: data.quote_lot_size,
+            fee_rate_bps: data.fee_rate_bps as u32,
+            vault_signer_nonce: data.vault_signer_nonce,
+            quote_dust_threshold: data.quote_dust_threshold
+        };
 
-    // info!("Serum Initialize Market");
-    // let dir_path = format!("./unknown-txs/{}", program_address);
-    // fs::create_dir_all(&dir_path).expect("Failed to create directories");
-
-    // let json = serde_json::to_string_pretty(&notification).expect("Failed to serialize notification");
-
-    // let file_path = format!("{}/{}.json", dir_path, "intializeMarket");
-
-    // if !Path::new(&file_path).exists() {
-    //     let mut file = File::create(&file_path).expect("Failed to create file");
-    //     file.write_all(json.as_bytes()).expect("Failed to write to file");
-    // }
+        tx.send(MpscMessage {
+            topic: SERUM_INITIALIZE_MARKET_UPDATE.to_string(),
+            payload: message.encode_to_vec()
+        }).expect("Failed to send MPSC Message.");
+    
+        info!(
+            "Processing {} instruction for {} {}/{} {}", 
+            Paint::magenta("INITIALIZE_MARKET"),
+            Paint::cyan("SERUM_PROGRAM"),
+            Paint::black(base),
+            Paint::black(quote),
+            Paint::red(is_official)
+        );
+        info!(
+            "{} Market ID: {}", 
+            Paint::red(">"), 
+            Paint::black(market_id_address)
+        );
+    }
 }
