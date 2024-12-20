3bfc8df6c1b9321933e7cbf281005325edda5800
gimpey <gimpey@pr0xy.io>
2024-12-20 03:24:28 +0000

    sending notification when token is migrating

--- src/instructions/pump_fun/trade.rs
+++ src/instructions/pump_fun/trade.rs
@@ -1,7 +1,13 @@
 /// # Pump Fun Trade Detection
+/// This module is responsible for detecting trades that occur within the `pump.fun`
+/// bonding curve. When a trade is detected, the bonding curve is updated with the
+/// new SOL & token reserves.
 /// 
 /// REQUIRES REDIS: TRUE
+/// - Tracked tokens.
 /// REQUIRES ZMQ: TRUE
+/// - Token migrations started.
+/// - Token bonding curve updates.
 
 use solana_transaction_status::{parse_accounts::ParsedAccount, UiPartiallyDecodedInstruction, UiTransactionStatusMeta};
 use tokio_tungstenite::tungstenite::Error as WsError;
@@ -27,6 +33,9 @@ pub mod spl_token {
 
 use spl_token::PumpFunBondingCurveUpdate;
 
+// This information can all be derived from the `pump.fun` global address.
+// (4wTV1YmiEkRvAtNtsSGPtUrqRYQMe5SKy2uB4Jjaxnjf) While rare, this could
+// technically be updated to use different values for future tokens.
 const INITIAL_LAMPORT_RESERVES: u64 = 30_000_000_000;
 const INITIAL_VIRTUAL_TOKEN_RESERVES: u64 = 1_073_000_000_000_000;
 const INITIAL_REAL_TOKEN_RESERVES: u64 = 793_100_000_000_000;
@@ -40,18 +49,6 @@ pub async fn trade_handler(
     tx: UnboundedSender<MpscMessage>
 ) -> Result<(), WsError> {
     let token_address = instruction.accounts.get(2).unwrap().to_string();
-
-    let mut conn = redis_pool.get().await.map_err(|e| {
-        WsError::Io(io::Error::new(io::ErrorKind::Other, format!("Failed to get Redis connection: {:?}", e)));
-    }).expect("Failed to get Redis connection.");
-
-    let is_tracked: bool = conn.sismember(TRACKED_TOKEN_ADDRESSES, &token_address).await.map_err(|e| {
-        WsError::Io(io::Error::new(io::ErrorKind::Other, format!("Failed to check if token is tracked: {:?}", e)));
-    }).unwrap_or(false);
-
-    if !is_tracked {
-        return Ok(());
-    }
 
     let bonding_curve = instruction.accounts.get(3).unwrap();
     let bonding_curve_index = accounts.iter().position(|x| x.pubkey == *bonding_curve).unwrap();
@@ -67,6 +64,26 @@ pub async fn trade_handler(
     let virtual_lamport_reserves = real_lamport_reserves + INITIAL_LAMPORT_RESERVES;
     let virtual_token_reserves = bonding_curve_token_balance + (INITIAL_VIRTUAL_TOKEN_RESERVES - INITIAL_TOKEN_TOTAL_SUPPLY);
     let real_token_reserves = bonding_curve_token_balance - (INITIAL_TOKEN_TOTAL_SUPPLY - INITIAL_REAL_TOKEN_RESERVES);
+
+    if real_token_reserves == 0 {
+        info!(
+            "{} has {} and will begin migration to Raydium.",
+            Paint::cyan(token_address.to_string()),
+            Paint::magenta("BONDED")
+        );
+    }
+
+    let mut conn = redis_pool.get().await.map_err(|e| {
+        WsError::Io(io::Error::new(io::ErrorKind::Other, format!("Failed to get Redis connection: {:?}", e)));
+    }).expect("Failed to get Redis connection.");
+
+    let is_tracked: bool = conn.sismember(TRACKED_TOKEN_ADDRESSES, &token_address).await.map_err(|e| {
+        WsError::Io(io::Error::new(io::ErrorKind::Other, format!("Failed to check if token is tracked: {:?}", e)));
+    }).unwrap_or(false);
+
+    if !is_tracked {
+        return Ok(());
+    }
 
     let message = PumpFunBondingCurveUpdate {
         token_address: token_address.clone(),
